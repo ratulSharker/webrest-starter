3fa5dd8f1e7fadd1123fb73f301ff1330d5d6288
Ratul sharker <sharker.ratul.08@gmail.com>
2022-07-05 17:27:30 +0600

    - docker compose file added. - default database mysql added. - db health check is added. made sure, unless db is healthy application should not startup. - for initial run, made sure sample data is present at startup. - unwanted sql file removed.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 -- MySQL
-INSERT INTO app_user (app_user_id, name, email, mobile, password, app_user_type, created_at, updated_at)
+INSERT IGNORE INTO app_user (app_user_id, name, email, mobile, password, app_user_type, created_at, updated_at)
 VALUES (1, "John Doe", "john@gmail.com", "+880123456789", "2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b", "ADMIN", CURDATE(), CURDATE());
