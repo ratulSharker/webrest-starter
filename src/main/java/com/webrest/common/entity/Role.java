0836fb2f815ddaf93a0788946b86db8537b485d8
Ratul Sharker <sharker.ratul.08@gmail.com>
2022-07-28 02:28:25 +0600

    - unwanted import removed.

--- src/main/java/com/webrest/common/entity/Role.java
+++ src/main/java/com/webrest/common/entity/Role.java
@@ -13,7 +13,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 
--- src/main/java/com/webrest/common/entity/RoleAuthorizationId.java
+++ src/main/java/com/webrest/common/entity/RoleAuthorizationId.java
@@ -1,7 +1,6 @@
 package com.webrest.common.entity;
 
 import java.io.Serializable;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
